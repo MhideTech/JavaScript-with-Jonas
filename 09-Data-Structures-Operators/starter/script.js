58c741323dc38c6888451330546e1b3a42b6b3d6
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-08-16 19:22:54 +0100

    Data Structure Ends but am still coming back

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -709,6 +709,8 @@ planesInLine(12);
 */
 
 // Data needed for a later exercise
+/*
+
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
@@ -718,3 +720,5 @@ for(const flight of flights.split('+')){
   const output = `${type.startsWith('_Delayed') ? '🛑' : ''}${type.replaceAll(/_/g, ' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(55);
   console.log(output);
 }
+
+*/
