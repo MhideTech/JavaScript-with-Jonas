d95423e9b2782ae3b0cd5241e34486458adafe64
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-08-17 11:33:27 +0100

    Default Parameters with Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+// Default Parameters with Functions
+const bookings = [];
+const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers){
+    // Setting default values => ES5
+    // numPassengers = numPassengers || 1;
+    // price = price || 199;
+
+    const booking = {
+      flightNum,
+      numPassengers,
+      price,
+    };
+    console.log(booking)
+    bookings.push(booking);
+}
+createBooking("LH123")
+createBooking("LH123", 2, 800)
+createBooking("LH123", 2)
+console.log(bookings);
