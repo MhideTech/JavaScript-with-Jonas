91d1b79ce841391c3518d023ffa13c18f30ccda5
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-08-22 02:03:41 +0100

    The new at Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -77,6 +77,8 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 
 // Simple Array Methods
+/*
+
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // SLICE
@@ -109,3 +111,19 @@ console.log([...arr, ...arr2]);
 
 // JOIN
 console.log(letters.join(' - ')); // print a string-like text with an hypen between every items in the array
+
+*/
+
+
+// The new at Method
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
