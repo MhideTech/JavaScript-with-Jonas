b3908e997349b8c12e4ea2a0b0f738cb6e6534d5
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-08-27 19:53:04 +0100

    flat and flatMap method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -484,6 +484,8 @@ for(const accoun of accounts){
 
 
 // The Some and Every Method
+/*
+
 console.log(movements);
 
 // Some Method
@@ -500,3 +502,27 @@ console.log(movements.some(mov => mov > 0));
 // The Every method returns true if all elements meet the required condition
 console.log(movements.every(mov => mov > 0));
 console.log(account4.movements.every(mov => mov > 0));
+
+*/
+
+
+// flat and flatMap
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// flat
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+// flatMap
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
+
