f06520a1c7c1d1e56882f0c1c1e34780060b5fe6
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-09-01 15:30:02 +0100

    ((Converting and Checking Numbers) && (Replace all Number() occurence with +))

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,42 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+// Converting and Checking Numbers
+
+// All Number in JavaScript are regarded as floating point Numbers
+console.log(23 === 23.0); // .: returns true
+
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+// Conversion and Parsing
+
+// Converting Strings to Numbers
+// Old Method
+console.log(Number('23'));
+// New Method
+console.log(+'23');
+
+// Parsing: retrieving numbers from a string
+// Inorder for parsing to work, the string needs to start with a number
+
+// parseInt => returns a whole number
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('px30', 10));
+
+// parseFloat => returns a floating-point number
+console.log(Number.parseInt('2.5rem')); // returns 2
+console.log(Number.parseFloat('2.5rem')); // returns 2.5
+
+// isNaN => checks if a value is not a number (returns true or false)
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20X'));
+console.log(Number.isNaN(23 / 0));
+
+// isFinite => checks if the value is a real number (opp of isNaN)
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20X'));
+console.log(Number.isFinite(23 / 0));
