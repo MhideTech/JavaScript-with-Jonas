72174971293a462c502dc3b488fa701351d04e49
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-09-02 14:45:41 +0100

    Creating dates end

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -426,3 +426,9 @@ console.log(new Date('Aug 02 2020 18:05:41'));
 console.log(new Date('December 24, 2015'));
 console.log(new Date(account1.movementsDates[0]));
 
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
