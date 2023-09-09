f9235d18bf6392e59e058e62f6f1aea18b4cc474
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-09-09 20:45:52 +0100

    Implementing Date to our bankist app

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -89,7 +89,13 @@ const displayMovements = function (acc, sort = false) {
   
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
-      
+    
+    const date = new Date(acc.movementsDates[i]);
+    const day = `${date.getDay() + 1}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    const displayDate = `${day}/${month}/${year}`;
+    
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
@@ -177,6 +183,15 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
+    // Create current time and date
+    const now = new Date();
+    const day = `${now.getDay() + 1}`.padStart(2, 0);
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const year = now.getFullYear();
+    const hour = `${now.getHours()}`.padStart(2, 0);
+    const min = `${now.getMinutes()}`.padStart(2, 0);
+    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -200,6 +215,10 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    // Add transfer date
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -213,6 +232,9 @@ btnLoan.addEventListener('click', function (e) {
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
     currentAccount.movements.push(amount);
+
+    // Add loan date
+    currentAccount.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -443,6 +465,7 @@ console.log(new Date(3 * 24 * 60 * 60 * 1000));
 
 
 // Working with dates
+/*
 
 const future = new Date(2037, 10, 19, 15, 23);
 console.log(future);
@@ -462,3 +485,5 @@ console.log(Date.now());
 
 future.setFullYear(2040);
 console.log(future);
+
+*/
