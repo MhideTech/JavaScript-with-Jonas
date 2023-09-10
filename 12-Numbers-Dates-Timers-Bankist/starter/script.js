99f362ff9c14c8dc3e1638cd549d442e2507faa6
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-09-10 18:23:35 +0100

    (Operation with date) && (Modifying movements date)

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -22,8 +22,8 @@ const account1 = {
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
     '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-09-06T23:36:17.929Z',
+    '2023-09-09T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -81,21 +81,37 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (acc, sort = false) {
-  containerMovements.innerHTML = '';
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
 
-  const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements;
-  
-  
-  movs.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
-    
-    const date = new Date(acc.movementsDates[i]);
+  if (daysPassed === 0) return `Today`;
+  if (daysPassed === 1) return `Yesterday`;
+  if (daysPassed <= 7) return `${daysPassed} days ago`
+  else {
     const day = `${date.getDay() + 1}`.padStart(2, 0);
     const month = `${date.getMonth() + 1}`.padStart(2, 0);
     const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
-    
+    return `${day}/${month}/${year}`;
+  }
+};
+
+const displayMovements = function (acc, sort = false) {
+  containerMovements.innerHTML = '';
+
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
+
+  movs.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(acc.movementsDates[i]);
+
+    const displayDate = formatMovementDate(date);
+
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
@@ -318,7 +334,6 @@ console.log(Number.isFinite(23 / 0));
 
 */
 
-
 // Math and Rounding
 /*
 
@@ -349,7 +364,6 @@ console.log((2.745).toFixed(2));
 console.log(+(2.745).toFixed(2)); // converting string to number
 
 */
-
 
 // The Remainder Operator
 /*
@@ -382,7 +396,6 @@ labelBalance.addEventListener('click', function () {
 
 */
 
-
 // Numeric Separators
 /*
 
@@ -403,7 +416,6 @@ console.log(Number('230_000')); // cant use with the number method (returns NaN)
 console.log(parseInt('230_000')); // returns only the number before the separator
 
 */
-
 
 // Working with BigInt
 /*
@@ -463,7 +475,6 @@ console.log(new Date(3 * 24 * 60 * 60 * 1000));
 
 */
 
-
 // Working with dates
 /*
 
@@ -487,3 +498,13 @@ future.setFullYear(2040);
 console.log(future);
 
 */
+
+// Operations with Dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+// Creating an arrow function dat calc the difference btw 2 days
+const calcDaysPasseds = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+const day1 = calcDaysPasseds(new Date(2037, 3, 14), new Date(2037, 3, 24));
+console.log(day1);
