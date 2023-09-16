18abfa0457790ff9a188b0d2d2e51ad9a3ef5650
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-09-16 03:21:29 +0100

    Creating Tabbed Components

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -241,6 +241,8 @@ document.querySelector(".nav__links").addEventListener('click', function(e){
 */
 
 // DOM Traversing
+/*
+
 const h1 = document.querySelector('h1');
 
 // Going downwards: child
@@ -266,3 +268,29 @@ console.log(h1.parentElement.children); // accessing all the sibling element thr
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
 
+*/
+
+// Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  // console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tabs
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add(`operations__content--active`);
+});
