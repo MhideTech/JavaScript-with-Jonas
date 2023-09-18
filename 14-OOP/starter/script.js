fc3a10734dd9d22f5afe73c8a64e1b91bfbe8960
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-09-18 21:49:56 +0100

    Principles of OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,3 +1,9 @@
 'use strict';
 
 // I learnt the introduction to Object Oriented Programming
+
+// Principles of OOP
+// 1. Abstraction: ignoring or hiding details that doesnt matter
+// 2. Encapsulation: keeping methods and properties private inside d class so that they are not accesible from outside the class
+// 3. Inheritance: making all properties and methods of a class (parent class) available to another class (child class)
+// 4. Polymorphism: A child class can overwrite a method it inherited from its parent class
