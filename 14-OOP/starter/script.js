9466146ce5afecddc8311483355ef5178d3b9a61
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-09-19 05:14:26 +0100

    OOP in JS: Prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -7,3 +7,8 @@
 // 2. Encapsulation: keeping methods and properties private inside d class so that they are not accesible from outside the class
 // 3. Inheritance: making all properties and methods of a class (parent class) available to another class (child class)
 // 4. Polymorphism: A child class can overwrite a method it inherited from its parent class
+
+// OOP in JS: Prototypes
+// All objects in JS are linked to a certain prototype object
+// Prototypal Inheritance: All objects that are linked to a certain prototype object can use the method & properties that are defined on that prototype
+
