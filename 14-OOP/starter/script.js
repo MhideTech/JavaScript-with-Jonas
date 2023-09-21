9e54a0a5fc8a5293743a9ae1aa96c7ab016a5154
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-09-21 21:42:54 +0100

    Object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -214,3 +214,29 @@ Person.hey = function(){
 Person.hey();
 PersonCl.hey();
 
+
+//////////////////////////////////////
+// Object.create
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear)
+    },
+
+    init(fullName, birthYear){
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+}
+
+const steven = Object.create(PersonProto);
+console.log(steven); // creates an empty object
+
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
