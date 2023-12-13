958c48f803d20b10cebf5cdc19874d5c7d7c9a0e
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-12-13 06:59:10 +0100

    The event loop in practice

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -188,6 +188,7 @@ btn.addEventListener('click', function () {
 */
 
 // coding challenge #1
+/*
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`).then(res => {
     if (!res.ok) throw new Error(`Problem with geocoding (${res.status})`)
@@ -212,3 +213,16 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
+
+
+// The event loop in practice
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res))
+// promise to further delay setTimeout callback
+Promise.resolve('Resolved promise 2').then(res => {
+  for(let i = 0; i < 1000; i++){}
+  console.log(res);
+});
+console.log('Test end');
