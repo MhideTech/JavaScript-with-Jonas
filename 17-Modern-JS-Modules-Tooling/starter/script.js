2ff8d7774346971b89ac02169f4645de61741976
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-12-22 08:48:15 +0100

    Top-Level await (ES2022)

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -26,3 +26,26 @@ import add, { addToCart, totalPrice as price, totalQuantity } from './shoppingCa
 console.log(price, totalQuantity);
 */
 
+
+// Top-Level await (ES2022);
+console.log('Start fetching');
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+const data = await res.json();
+console.log(data);
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data  = await res.json();
+  
+  return {title: data.at(-1).title, text: data.at(-1).body};
+}
+
+const lastPost = getLastPost();
+console.log(lastPost); // returns a promise 'cause getLastPost is an async function that hasnt resolved yet
+
+// Not very clean
+lastPost.then(last => console.log(last));
+
+// Better alternative
+const lastPost2 = await getLastPost();
+console.log(lastPost2)
