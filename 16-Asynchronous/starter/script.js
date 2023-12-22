5ea27abfc8fa015c161292faa9e6fc03f1d1cd20
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-12-22 01:33:02 +0100

    Running promises in parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -353,6 +353,7 @@ createImage('img/img-1.jpg')
 
 
 // Consuming promises with AsyncAwait
+/*
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -390,3 +391,27 @@ const whereAmI = async function () {
 };
 
 whereAmI();
+*/
+
+
+// Running promises in parallel
+const get3Countries = async function(c1, c2, c3){
+  try{
+    // const [data1] = await getJSON(`https://restcountries.eu/rest/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.eu/rest/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.eu/rest/v2/name/${c3}`);
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch(e){
+    console.log(e);
+  }
+}
+
+get3Countries('portugal', 'nigeria', 'canada')
+
