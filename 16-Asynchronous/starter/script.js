16ff695875e9bdd3b42760218b6a91ff88bbb745
Mhizta-orlah <okefolahanolamide2006@gmail.com>
2023-12-22 01:36:29 +0100

    Solved coding challenge #3 && Finished Asynchronous JavaScript

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -419,6 +419,7 @@ get3Countries('portugal', 'nigeria', 'canada')
 
 
 // Other promise combinators: race, allSettled and any
+/*
 // Promise.race
 (async function() {
   const res = await Promise.race([
@@ -462,4 +463,66 @@ Promise.any([
   Promise.reject('ERROR'),
   Promise.resolve('Another success')
 ]).then(res => console.log(res)).catch(e => console.error(e));
+
+*/
+
+
+
+// Coding Challenge #3
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+const loadNPause = async function(imgPath){
+  try{
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 Loaded');
+    await wait(2);
+    img.style.display = 'none';
+
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 Loaded');
+    await wait(2);
+    img.style.display = 'none';
+  } catch(e){
+    console.error(e)
+  }
+}
+
+// loadNPause();
+
+const loadAll = async function(imgArr) {
+  try{
+    let imgs = imgArr.map(async img => await createImage(img));
+    const imgsEl = await Promise.all(imgs)
+    console.log(imgsEl)
+    imgsEl.forEach(img => img.classList.add('parallel'));
+  } catch(err){
+    console.error(err)
+  }
+}
+
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
+
 
